a1abf67d98497d93c03c3dde87a9dade1afc4cf8
Adriano Fantini <adr.fantini@gmail.com>
2019-02-24 16:07:33 +0100

    Complete refactor, removing stars and raster. Reading and writing now purely done via ncdf4. Fixes #1. Fixes #2.

--- ncspellR.R
+++ ncspellR.R
@@ -5,24 +5,23 @@ description = 'Script to calculate the yearly spell count for an index from a mo
 The output will contain the number of events for each year in the input.
 A spell event starts when two consecutive timesteps go under a given threshold, and stops when another higher threshold is surpassed.
 The year of any given event is marked as when the event started, not when it ended.'
-further_description = 'Input file MUST be monthly. Does everything in memory, so make sure your dataset fits in memory! \nVery few checks are performed, so also make sure you know what you are doing.\n Input files must follow the CF Conventions >= 1.5 (http://cfconventions.org/).'
+further_description = 'Input file MUST be monthly. Does everything in memory, so make sure your dataset fits in memory! \nVery few checks are performed, so also make sure you know what you are doing.\n Input files must follow the CF Conventions >= 1.5 (http://cfconventions.org/).\n This program is parallel by default, but is not capable of crossing node boundaries (cannot currently run on multiple nodes).'
 author = 'Adriano Fantini'
-version = '0.1'
+version = '0.2'
 contact = 'afantini@ictp.it'
 gh_url = 'https://github.com/adrfantini/ncspellR'
 required_pkgs = c(
     'pacman',
     'optparse',
     'glue',
-    'stars',
     'magrittr',
     'futile.logger',
     'lubridate',
-    'raster'
+    'ncdf4',
+    'PCICt',
+    'pbapply'
 )
 version_pkgs = c( # Set requirements for package versions
-    stars = '0.3.0',
-    sf = '0.7.3'
 )
 
 #============= INITIALIZATION =============
@@ -52,7 +51,7 @@ flog.fatal = function(...) {futile.logger::flog.fatal(...); fatalerror()}
 option_list = list( make_option(c("-n", "--nthreads"),
                                 type="integer",
                                 default=NA,
-                                help="Number of threads to be used. Set to NA for automatic detection (all available threads will be used) [default: %default]"),
+                                help="Number of threads to be used. Set to NA for automatic detection (all available threads in the node will be used) [default: %default]"),
                     make_option(c("-v", "--varin"),
                                 type="character",
                                 default='SPI',
@@ -73,9 +72,15 @@ option_list = list( make_option(c("-n", "--nthreads"),
                                 type="character",
                                 default=NULL,
                                 help="Optional file to write logs to. [default: %default]"),
+                    make_option("--progress",
+                                action="store_true",
+                                help="Show a progress bar - this slightly decreases performance"),
+                    make_option("--compress",
+                                action="store_true",
+                                help="Activate netCDF compression (with deflate level 1) for the main variable"),
                     make_option("--debug",
                                 action="store_true",
-                                help="Print additional debug output. This flag is also useful if you want to check that the options were correctly understood [default: %default]")
+                                help="Print additional debug output. This flag is also useful if you want to check that the options were correctly understood")
                     )
 parser = OptionParser(
     usage = "%prog [options] INPUT OUTPUT",
@@ -89,7 +94,7 @@ parser = OptionParser(
     {further_description}
 
     REQUIRED PACKAGES: {paste(required_pkgs, collapse=', ')}
-    MISSING: {missing}
+    MISSING  PACKAGES: {missing}
 
     Note that {program_name} will *attempt* to install any missing packages by itself.
 
@@ -112,22 +117,36 @@ logfile = opt$logfile
 if (!is.null(logfile)) {
     if (!file.exists(logfile)) ftry(file.create(logfile))
     if (file.access(logfile, mode=2) == -1) {
-        flog.fatal('cannot write to log file, check permissions and path')
+        flog.fatal('Cannot write to log file, check permissions and path')
     }
-    flog.appender(appender.tee(logfile))
+    invisible(flog.appender(appender.tee(logfile)))
+    if (file.exists(logfile)) flog.debug('Appending output to logfile %s', logfile)
 }
+
+flog.info(glue(' ### Starting {program_name} version {version} from {author} ({contact}) ### '))
+
+progress = isTRUE(opt$progress)
+if (progress) {
+    flog.debug('Progress bar will be shown')
+} else {
+    flog.debug('Progress bar will not be shown')
+}
+
+compress = isTRUE(opt$compress)
+deflate_level = 1
+if (compress) flog.debug('Deflate compression activated')
 
 flog.debug("Parsing input arguments")
 
 fn_in = arguments$args[1]
 flog.debug("Input file: %s", fn_in)
-if (!file.exists(fn_in)) flog.fatal('input file does not exist')
-if (file.access(fn_in, mode=4) == -1) flog.fatal('input file cannot be read, check permissions and path')
+if (!file.exists(fn_in)) flog.fatal('Input file does not exist')
+if (file.access(fn_in, mode=4) == -1) flog.fatal('Input file cannot be read, check permissions and path')
 
 fn_out = arguments$args[2]
 flog.debug("Output file: %s", fn_out)
-if (file.exists(fn_out)) flog.fatal('input file already exists')
-if (file.access(dirname(fn_in), mode=2) == -1) flog.fatal('output file cannot be created, check permissions and path')
+if (file.exists(fn_out)) flog.fatal('Output file already exists')
+if (file.access(dirname(fn_in), mode=2) == -1) flog.fatal('Output file cannot be created, check permissions and path')
 
 var_in = opt$varin
 flog.debug("Input variable name: %s", var_in)
@@ -159,25 +178,80 @@ flog.debug("Loaded packages")
 #============= READ INPUT DATA =============
 
 flog.info("Reading metadata")
-nc_in = suppressWarnings(fn_in %>% read_stars(sub = var_in, proxy = TRUE))
+nc_in = nc_open(fn_in)
 
-if (!st_dimensions(nc_in)$time$refsys %in% c('PCICt', 'POSIXct', 'POSIXlt')) {
-    flog.fatal('Input calendar not understood. Are you sure your input file follows the CF Conventions?')
+# Read time
+time_var = 'time'
+times = nc_in %>% ncvar_get(time_var)
+time_units = nc_in %>% ncatt_get(time_var, 'units')
+if (!time_units$hasatt) flog.fatal('Cannot find time units!')
+flog.debug('Time units: %s', time_units$value)
+time_units = strsplit(time_units$value, " ")[[1]]
+time_cal = nc_in %>% ncatt_get(time_var, 'calendar')
+if (!time_cal$hasatt) {
+    flog.info('No input calendar, assuming standard')
+    time_cal = 'gregorian'
+} else {
+    time_cal = time_cal$value %>% tolower
+    flog.debug('Input calendar: %s', time_cal)
 }
-times = suppressWarnings(st_get_dimension_values(nc_in, 'time') %>% as.POSIXct %>% round('day'))
+
+pcict_calendars = c('365', '365_day', 'noleap', '360', '360_day', 'gregorian', 'proleptic_gregorian', 'standard')
+if (time_cal %in% pcict_calendars) {
+    # Use PCIct to deal with times
+    time_offset_unit <- time_units[1]
+    time_tz <- time_units[5]
+    time_start <- strsplit(time_units[4], ":")[[1]]
+    if (length(time_start) != 3 || time_start[1] > 24 || time_start[2] > 60 || time_start[3] > 60 || any(time_start < 0)) {
+        flog.warn("%s is not a valid start time. Assuming 00:00:00", time_start)
+        time_units[4] <- "00:00:00"
+    }
+    if (! time_tz %in% OlsonNames()) {
+        flog.debug("%s is not a valid timezone. Assuming UTC", time_tz)
+        time_tz <- "UTC"
+    }
+    time_start <- ymd_hms(paste(time_units[3], time_units[4]), tz=time_tz)
+    # Find the correct lubridate time function based on the unit
+    time_f <- switch(tolower(time_offset_unit),
+        seconds=seconds, second=seconds, sec=seconds,
+        minutes=minutes, minute=minutes, min=minutes,
+        hours=hours,     hour=hours,     h=hours,
+        days=days,       day=days,       d=days,
+        months=months,   month=months,   m=months,
+        years=years,     year=years,     yr=years,
+        NA
+    )
+    times = as.PCICt(time_start, cal=time_cal) + as.numeric(time_f(floor(times))) # TODO this floor here is a hack, to work around files which have non-integer times. Works in most cases.
+} else {
+    flog.fatal('This program does not support calendar ""%s"', time_cal)
+}
+
+# Simplify dates, since we only care about months
+times = suppressWarnings(times %>% as.POSIXct %>% round('day'))
 years = year(times)
 r_years = years %>% rle
 lr_years = r_years$lengths %>% length
 check12 = all( r_years$lengths[c(-1, -lr_years)] == 12 )
-if (!check12) flog.fatal('Some internal years were detected to not have exactly 12 timesteps. Missing any timesptes?')
+if (!check12) flog.fatal('Some internal years were detected to not have exactly 12 timesteps. Missing any month?')
 first_year = years[1]
 last_year = years[length(years)]
 if (any( unique(years) != (first_year:last_year) )) flog.fatal('Years appear to not be consecutive. Missing any years?')
 flog.debug("First year in the file: %d", first_year)
 flog.debug("Last  year in the file: %d", last_year)
 
+# Function to get dimensions for a given variable
+ncdim_get = function(nc, varid) {
+    nc$var[[varid]]$dim %>% sapply(`[[`, 'name')
+}
+
+# Inspect input variable to read
+nc_var_in = nc_in$var[[var_in]]
+if (is.null(nc_var_in)) flog.fatal('Input file %s does not contain variable %s', fn_in, var_in)
+nc_var_in_dims = nc_in %>% ncdim_get(var_in)
+
+# Read data
 flog.info("Reading data")
-nc_in = suppressWarnings(fn_in %>% read_stars(sub = var_in, proxy = FALSE))
+spi_in = nc_in %>% ncvar_get(var_in)
 
 #============= COMPUTE =============
 
@@ -221,30 +295,125 @@ if (nthreads > 1 ) {
 }
 
 flog.info("Starting computation using %d threads", nthreads)
-spell_res = st_apply(nc_in,
-    which((st_dimensions(nc_in) %>% names) != 'time'),
-    spi_spell, CLUSTER = cluster, PROGRESS = TRUE,
+if (progress) {
+    pboptions('type' = 'timer')
+} else {
+    pboptions('type' = 'none')
+}
+spell_out = pbapply(spi_in,
+    which(nc_var_in_dims != 'time'),
+    spi_spell, cl = cluster,
     yrs = years, eventstart = estart, eventend = eend
 )
 if (nthreads > 1 ) stopCluster(cluster)
-names(spell_res) = var_out
 flog.info("Ended computation")
 
-# For some reason it is necessary to permutate the output. Further checks are necessary to verify this is the correct permutation # TODO
+# Repermutate the output which has been swapped by apply
 shift_vec = function(v) {
     len = length(v)
     return(c(v[2:len], v[1]))
 }
-spell_res = aperm(spell_res, shift_vec(1:length(dim(spell_res))))
+spell_out = aperm(spell_out, shift_vec(1:length(dim(spell_out))))
 
 #============= WRITE OUTPUT DATA =============
 
-# Better output writing # TODO
+# Get dimensions and variables to be cloned from the source file
+# coordinates and grid_mapping are special variables that act as pointers to other variables/dimensions
+nc_coords = nc_in %>% ncatt_get(var_in, 'coordinates')
+nc_grid_mapping = nc_in %>% ncatt_get(var_in, 'grid_mapping')
+vars2copy = NULL
+if (nc_coords$hasatt) {
+    flog.debug('Found "coordinates" attribute for variable %s', var_in)
+    vars2copy = nc_coords$value %>% strsplit(' ') %>% unlist
+}
+if (nc_grid_mapping$hasatt){
+    flog.debug('Found "grid_mapping" attribute for variable %s', var_in)
+    vars2copy = c(vars2copy, nc_grid_mapping$value %>% strsplit(' ') %>% unlist)
+}
+dims2copy = c(nc_var_in_dims, lapply(vars2copy, function(v) nc_in %>% ncdim_get(v)) %>% unlist) %>% unique
+flog.debug("Variables  to be cloned: {paste(vars2copy, collapse=', ')}" %>% glue)
+flog.debug("Dimensions to be cloned: {paste(dims2copy, collapse=', ')}" %>% glue)
 
-flog.info('Writing to file %s', fn_out)
-spell_res %>%
-    as('Raster') %>%
-    setZ(ymd(paste0(unique(years), '06', '15'))) %>%
-    writeRaster(fn_out, varname = var_out, varunit = '1', zname = 'time')
+# Creating output file
+flog.info('Initialising output file')
+# Remove time from output dimensions since we need to redefine out own
+nc_var_out_dims = nc_var_in_dims[which(nc_var_in_dims != 'time')]
+nc_var_out_time = ncdim_def(
+    name = 'time',
+    units = 'years since {first_year}-06-15 00:00:00' %>% glue,
+    vals = unique(years) - first_year,
+    unlim = TRUE,
+    create_dimvar = TRUE,
+    calendar = 'standard',
+    longname = 'time'
+)
 
-flog.info('All done')
+nc_var_out = ncvar_def(
+    var_out, '1', c(nc_in$dim[nc_var_out_dims], list(nc_var_out_time)),
+    longname='Yearly spell count',
+#     shuffle = compress,
+    compression = ifelse(compress, deflate_level, NA)
+)
+# It is here necessary to reset chunksizes to NA, in some cases, possibly due to a bug in ncdf4
+nc_in_vars2copy = lapply(nc_in$var[vars2copy], function(x) {x$chunksizes = NA; x})
+nc_out = fn_out %>% nc_create(c(list(nc_var_out), nc_in_vars2copy), force_v4 = TRUE)
+
+# Wrapper for ncatt_put to log in debug
+ncatt_put = function(nc, varid, attname, attval, ...) {
+    flog.debug('Setting %s attribute: %s = %s', ifelse(varid == 0, 'global', varid), attname, attval)
+    ncdf4::ncatt_put(nc, varid, attname, attval, ...)
+}
+
+# Add time standard name
+nc_out %>% ncatt_put('time', 'standard_name', 'time')
+
+# Clone all the attributes from the relevant variables and dimensions in the source file, excluding time
+for (v in c('global attributes', dims2copy[which(dims2copy != 'time')], vars2copy[which(vars2copy != 'time')])) {
+    if (v == 'global attributes') v = 0
+    if (v == 0) {
+        flog.debug('Getting global attributes')
+    } else {
+        flog.debug('Getting attributes for variable %s', v)
+    }
+
+    atts = nc_in %>% ncatt_get(v)
+    for (a in names(atts)) {
+        nc_out %>% ncatt_put(v, a, atts[[a]])
+    }
+}
+
+# Update netCDF history
+nc_h = ncatt_get(nc_in, 0, 'history')
+nc_new_h = '{date()}: yearly spell count calculated by {program_name} version {version} ({gh_url})' %>% glue
+if (nc_h$hasatt) {
+    nc_out %>% ncatt_put(0, 'history', paste(nc_new_h, nc_h$value, sep='\n'))
+} else {
+    nc_out %>% ncatt_put(0, 'history', nc_new_h)
+}
+
+# Fill in cloned variables
+for (v in vars2copy) {
+    flog.debug('Copying values for cloned variable %s', v)
+    nc_out %>% ncvar_put(v, nc_in %>% ncvar_get(v))
+}
+flog.debug('Closing input file')
+nc_close(nc_in)
+
+# Clone coordinates and grid_mapping attributes, if present
+if (nc_coords$hasatt) nc_out %>% ncatt_put(var_out, 'coordinates', nc_coords$value)
+if (nc_grid_mapping$hasatt) nc_out %>% ncatt_put(var_out, 'grid_mapping', nc_grid_mapping$value)
+
+# Fill SPI variable with data and attributes
+flog.info('Filling output file')
+nc_out %>% ncvar_put(var_out, spell_out)
+nc_out %>% ncatt_put(var_out, 'event_start', estart)
+nc_out %>% ncatt_put(var_out, 'event_end'  , eend)
+nc_out %>% ncatt_put(var_out, 'program', '{program_name} version {version} ({gh_url})' %>% glue)
+nc_out %>% ncatt_put(var_out, 'R_version', p_ver(R) %>% as.character)
+nc_out %>% ncatt_put(var_out, 'ncdf4_pkg_version', p_ver(ncdf4) %>% as.character)
+
+# Close
+flog.debug('Closing output file')
+nc_close(nc_out)
+flog.info(' ### Goodbye! ### ')
+
